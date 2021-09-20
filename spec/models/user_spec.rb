c55f36d71d904319ff6b7b68b7266c74f81f6ac2
Tetsuya Azami <zqianjian35@gmail.com>
2021-09-20 10:59:40 +0900

    remove rails db:migrate from ci

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-it "nameとemailを持ったユーザーを登録できること" do
-user = User.new(
-  name: "John",
-  email: "John@example.com"
-)
+  it 'nameとemailを持ったユーザーを登録できること' do
+    user = described_class.new(
+      name: 'John',
+      email: 'John@example.com'
+    )
 
-expect(user).to be_valid
-end
+    expect(user).to be_valid
+  end
 end
